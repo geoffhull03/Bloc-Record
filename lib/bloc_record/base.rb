03873860eea055dcb00ba442ed18c67439ac2415
Geoff Hull <geoffhull03@gmail.com>
2017-12-28 13:41:47 -0800

    Finished Checkpoint 7

--- lib/bloc_record/base.rb
+++ lib/bloc_record/base.rb
@@ -4,6 +4,8 @@ require 'bloc_record/persistence'
 require 'bloc_record/selection'
 require 'bloc_record/connection'
 require 'bloc_record/collection'
+require 'bloc_record/associations'
+
 
 module BlocRecord
   class Base
@@ -11,6 +13,7 @@ module BlocRecord
     extend Selection
     extend Schema
     extend Connection
+    extend Associations
   end
 
   def initialize(options={})
